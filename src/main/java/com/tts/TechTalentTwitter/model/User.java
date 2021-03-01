4397d268ea5e161adeea17d298a872db6a536e3b
Joey Whalen <jtktwhalen@gmail.com>
2021-03-01 11:09:59 -0500

    Updated User.java

--- src/main/java/com/tts/TechTalentTwitter/model/User.java
+++ src/main/java/com/tts/TechTalentTwitter/model/User.java
@@ -19,6 +19,9 @@ import javax.validation.constraints.Pattern;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -39,12 +42,15 @@ public class User {
 	@NotEmpty(message = "Please provide an email")
 	private String email;
 
+	@NotEmpty(message = "Please provide a username")
 	@Length(min = 3, message = "Your username must have at least 3 characters")
 	@Length(max = 15, message = "Your username cannot have more than 15 characters")
 	@Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
 	private String username;
 
 	@Length(min = 5, message = "Your password must have at least 5 characters")
+	@NotEmpty(message = "Please provide a password")
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private String password;
 
 	@NotEmpty(message = "Please provide your first name")
@@ -52,6 +58,7 @@ public class User {
 
 	@NotEmpty(message = "Please provide your last name")
 	private String lastName;
+
 	private int active;
 
 	@CreationTimestamp
