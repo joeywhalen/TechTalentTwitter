b04edde443e805b29ec5604b713b613db4f943f2
Joey Whalen <jtktwhalen@gmail.com>
2021-03-03 20:14:20 -0500

    Updated TagRepository

--- src/main/java/com/tts/TechTalentTwitter/repository/TagRepository.java
+++ src/main/java/com/tts/TechTalentTwitter/repository/TagRepository.java
@@ -1,5 +1,12 @@
 package com.tts.TechTalentTwitter.repository;
 
-public interface TagRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tts.TechTalentTwitter.model.Tag;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Long> {
+	Tag findByPhrase(String phrase);
 
 }
