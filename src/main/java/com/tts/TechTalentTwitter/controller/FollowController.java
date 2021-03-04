06469947619320c147daf172e3dc4b038e2775bf
Joey Whalen <jtktwhalen@gmail.com>
2021-03-03 20:13:23 -0500

    Updated FollowController

--- src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/FollowController.java
@@ -3,9 +3,8 @@ package com.tts.TechTalentTwitter.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-
+import com.tts.TechTalentTwitter.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
