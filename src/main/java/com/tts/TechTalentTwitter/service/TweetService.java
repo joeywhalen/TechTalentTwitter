ceac07a41803e23072108bf5d8bb86952ec5cd31
Joey Whalen <jtktwhalen@gmail.com>
2021-03-04 16:13:31 -0500

    Updated TweetService

--- src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
+++ src/main/java/com/tts/TechTalentTwitter/service/TweetService.java
@@ -1,17 +1,22 @@
 package com.tts.TechTalentTwitter.service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import java.util.concurrent.TimeUnit;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tts.TechTalentTwitter.model.Tag;
 import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.TweetDisplay;
 import com.tts.TechTalentTwitter.model.User;
 import com.tts.TechTalentTwitter.repository.TagRepository;
 import com.tts.TechTalentTwitter.repository.TweetRepository;
@@ -24,22 +29,22 @@ public class TweetService {
 	@Autowired
 	private TagRepository tagRepository;
 
-	public List<Tweet> findAll() {
+	public List<TweetDisplay> findAll() {
 		List<Tweet> tweets = tweetRepository.findAllByOrderByCreatedAtDesc();
 		return formatTweets(tweets);
 	}
 
-	public List<Tweet> findAllByUser(User user) {
+	public List<TweetDisplay> findAllByUser(User user) {
 		List<Tweet> tweets = tweetRepository.findAllByUserOrderByCreatedAtDesc(user);
-		return tweets;
+		return formatTweets(tweets);
 	}
 
-	public List<Tweet> findAllByUsers(List<User> users) {
+	public List<TweetDisplay> findAllByUsers(List<User> users) {
 		List<Tweet> tweets = tweetRepository.findAllByUserInOrderByCreatedAtDesc(users);
-		return tweets;
+		return formatTweets(tweets);
 	}
 	
-	public List<Tweet> findAllWithTag(String tag) {
+	public List<TweetDisplay> findAllWithTag(String tag) {
 	    List<Tweet> tweets = tweetRepository.findByTags_PhraseOrderByCreatedAtDesc(tag);
 	    return formatTweets(tweets);
 	  }
@@ -50,7 +55,7 @@ public class TweetService {
 	}
 	
 	private void handleTags(Tweet tweet) {
-	    List<Tag> tags = new ArrayList<>();
+	    List<Tag> tags = new ArrayList<Tag>();
 	    Pattern pattern = Pattern.compile("#\\w+");
 	    Matcher matcher = pattern.matcher(tweet.getMessage());
 	    while (matcher.find()) {
@@ -66,18 +71,41 @@ public class TweetService {
 	    tweet.setTags(tags);
 	  }
 
-	  private List<Tweet> formatTweets(List<Tweet> tweets) {
+	  private List<TweetDisplay> formatTweets(List<Tweet> tweets) {
 	    addTagLinks(tweets);
 	    shortenLinks(tweets);
-	    return tweets;
+	    List<TweetDisplay> displayTweets = formatTimestamps(tweets);
+	    return displayTweets;
 	  }
+	  
+	  private List<TweetDisplay> formatTimestamps(List<Tweet> tweets) {
+	        List<TweetDisplay> response = new ArrayList<>();
+	        PrettyTime prettyTime = new PrettyTime();
+	        SimpleDateFormat simpleDate = new SimpleDateFormat("M/d/yy");
+	        Date now = new Date();
+	        for (Tweet tweet : tweets) {
+	            TweetDisplay tweetDisplay = new TweetDisplay();
+	            tweetDisplay.setUser(tweet.getUser());
+	            tweetDisplay.setMessage(tweet.getMessage());
+	            tweetDisplay.setTags(tweet.getTags());
+	            long diffInMillies = Math.abs(now.getTime() - tweet.getCreatedAt().getTime());
+	            long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+	            if (diff > 3) {
+	                tweetDisplay.setDate(simpleDate.format(tweet.getCreatedAt()));
+	            } else {
+	                tweetDisplay.setDate(prettyTime.format(tweet.getCreatedAt()));
+	            }
+	            response.add(tweetDisplay);
+	        }
+	        return response;
+	    }
 
 	  private void addTagLinks(List<Tweet> tweets) {
 	    Pattern pattern = Pattern.compile("#\\w+");
 	    for (Tweet tweet : tweets) {
 	      String message = tweet.getMessage();
 	      Matcher matcher = pattern.matcher(message);
-	      Set<String> tags = new HashSet<>();
+	      Set<String> tags = new HashSet<String>();
 	      while (matcher.find()) {
 	        tags.add(matcher.group());
 	      }
